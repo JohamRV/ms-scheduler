593fc418e6ffabf64600ef26dc52b8e3002c3f4d
Joham Romucho <joham.romucho@pucp.edu.pe>
2023-07-09 20:05:16 -0500

    feat: set up schedular microservice

---
+++ src/main/java/pe/edu/pucp/msschedular/MsSchedularApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSchedularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSchedularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/msschedular/MsSchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msschedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSchedularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
